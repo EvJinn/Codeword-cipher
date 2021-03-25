ffc9fd3089ba532249e22e0cbfe0af2e8b1e77bf
Евгений Копылов <zhenya.ko1711@gmail.com>
2021-03-25 15:02:05 +0700

    Добавьте файлы проекта.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите кодовое слово:");
+            string codeWord = Console.ReadLine();
+
+            cipher cipher = new cipher(codeWord);
+
+            Console.WriteLine(string.Join('\0', cipher.firstAlphabet));
+            Console.WriteLine(string.Join('\0', cipher.secondAlphabet));
+
+            Console.WriteLine("Что делаем?: \n" +
+                                "1. Шифруем \n" +
+                                "2. Дешифруем \n");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите текст:");
+            
+            switch (n)
+            {
+                case 1:
+                    {
+                        string text = Console.ReadLine();
+
+                        string cipherText = cipher.encode(text);
+                        Console.WriteLine(cipherText);
+                    }
+                    break;
+                case 2:
+                    {
+                        string cipherText = Console.ReadLine();
+
+                        string decodedText = cipher.decode(cipherText);
+                        Console.WriteLine(decodedText);
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
---
+++ ConsoleApp3/cipher.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace ConsoleApp3
+{
+    public class cipher
+    {
+        public readonly IEnumerable<char> firstAlphabet = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя".ToCharArray().AsEnumerable();
+        public readonly IEnumerable<char> secondAlphabet;
+
+        public cipher(string codeWord)
+        {
+            var x = codeWord.Distinct();
+            secondAlphabet = x.Concat(firstAlphabet).Distinct();
+        }
+
+        public string encode(string text)
+        {
+            var cipherText = new List<char>();
+            for (int i = 0; i < text.Length; i++)
+            {
+                char symb = text[i];
+                for (int j = 0; j < firstAlphabet.Count(); j++)
+                    if (symb == firstAlphabet.ElementAt(j)) cipherText.Add(secondAlphabet.ElementAt(j));
+                if (symb == ' ') cipherText.Add(' ');
+            }
+
+            return string.Join('\0', cipherText);
+        }
+
+        public string decode(string cipherText)
+        {
+            var decodedText = new List<char>();
+            for (int i = 0; i < cipherText.Length; i++)
+            {
+                char symb = cipherText[i];
+                for (int j = 0; j < secondAlphabet.Count(); j++)
+                    if (symb == secondAlphabet.ElementAt(j)) decodedText.Add(firstAlphabet.ElementAt(j));
+                if (symb == ' ') decodedText.Add(' ');
+            }
+
+            return string.Join('\0', decodedText);
+        }
+    }
+}
